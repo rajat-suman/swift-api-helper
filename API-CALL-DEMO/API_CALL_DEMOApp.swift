8845eda74fc9cd202492e7a41af29a7f0f5798f9
Rajat Suman <rajat@future-doctor.de>
2024-07-29 10:08:55 +0530

    Initial Commit

---
+++ API-CALL-DEMO/API_CALL_DEMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CALL_DEMOApp.swift
+//  API-CALL-DEMO
+//
+//  Created by Rajat Suman on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct API_CALL_DEMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
